885a7a253cce9dbede01f316b1e771b54a2bd7c3
webhzp <1225613448@qq.com>
2019-04-23 19:32:33 +0800

    路由

---
+++ src/components/classify/classify.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Classify">
+        分类
+    </div>
+</template>
+
+<script>
+    name:'classify'
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
