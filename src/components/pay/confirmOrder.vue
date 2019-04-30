49871cca53671c696fcd7f092406ecf23f09c175
webhzp <1225613448@qq.com>
2019-04-30 18:42:02 +0800

    x

--- src/components/pay/confirmOrder.vue
+++ src/components/pay/confirmOrder.vue
@@ -158,7 +158,7 @@
         available: 1,
         condition: '无使用门槛\n最多优惠12元',
         reason: '',
-        value: 150,
+        value: 250,
         name: '优惠券名称',
         startAt: 1489104000,
         endAt: 1514592000,
