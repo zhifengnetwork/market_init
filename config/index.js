f97b5cf4c7d5e18eb5fe9f9f7570633768865e02
webhzp <1225613448@qq.com>
2019-05-20 14:41:04 +0800

    xx

--- config/index.js
+++ config/index.js
@@ -22,8 +22,11 @@ module.exports = {
 		},
 
     // Various Dev Server settings
-    host: 'zf_shop.zhifengwangluo.com', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    //host: 'http://zf_shop.zhifengwangluo.com/', // can be overwritten by process.env.HOST
+    //port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    
+    host: '192.168.1.112', // can be overwritten by process.env.HOST
+    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
