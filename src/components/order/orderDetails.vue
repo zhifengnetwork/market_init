83f0e891c0dd0237cec5159a00ce297f5db40ed8
16539693705 <1659693705@qq.com>
2019-05-20 17:45:08 +0800

    LL

--- src/components/order/orderDetails.vue
+++ src/components/order/orderDetails.vue
@@ -197,6 +197,10 @@
             })
         },
         methods: {
+            //将监听操作写在methods里面，removeEventListener取消监听内容必须跟开启监听保持一致，
+             fun(){
+                     this.$router.push('/user')
+             }
         },
         filters: {
             formatDate: function (value) {
@@ -215,7 +219,18 @@
                 s = s < 10 ? ('0' + s) : s;
                 return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
             }
-            }
+            }, //挂载完成后，判断浏览器是否支持popstate
+        mounted(){
+        if (window.history && window.history.pushState) {
+            history.pushState(null, null, document.URL);
+            window.addEventListener('popstate', this.fun, false);//false阻止默认事件
+          }
+        },
+        //页面销毁时，取消监听。否则其他vue路由页面也会被监听
+            destroyed(){
+        window.removeEventListener('popstate', this.fun, false);//false阻止默认事件
+        },
+
     }
 </script>
 
--- src/components/pay/paySucceed.vue
+++ src/components/pay/paySucceed.vue
@@ -75,23 +75,6 @@
                 this.orderData = JSON.parse(this.$route.params.list)
                 // console.log(this.$route.query.list)
         },
-        //挂载完成后，判断浏览器是否支持popstate
-        mounted(){
-        if (window.history && window.history.pushState) {
-            history.pushState(null, null, document.URL);
-            window.addEventListener('popstate', this.fun, false);//false阻止默认事件
-          }
-        },
-        //页面销毁时，取消监听。否则其他vue路由页面也会被监听
-            destroyed(){
-        window.removeEventListener('popstate', this.fun, false);//false阻止默认事件
-        },
-        //将监听操作写在methods里面，removeEventListener取消监听内容必须跟开启监听保持一致，
-        methods: {
-                    fun(){
-                     this.$router.push('/user')
-             }
-        },
         
     }
 </script>
